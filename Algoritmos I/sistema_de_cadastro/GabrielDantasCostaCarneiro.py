0a8f15c9d9488fa21132b4a408aa1e9d9f719f7c
Gabriel Dantas <98698672+B1ELL20@users.noreply.github.com>
2026-06-03 22:51:26 -0300

    feat: melhorias
    Modularização consistente
    Funções de validação de strings e inteiros
    Adição da classe History
    Mudança no Palyer, fetch_players e register_match para tratar history
    Ajuste no algoritmo de ordenação

--- Algoritmos I/sistema_de_cadastro/GabrielDantasCostaCarneiro.py
+++ Algoritmos I/sistema_de_cadastro/GabrielDantasCostaCarneiro.py
@@ -13,22 +13,61 @@ system_on = True
 PLAYERS_ARCHIVE = BASE_DIR / "jogadores.json"
 MATCHS_ARCHIVE = BASE_DIR / "partidas.json"
 
+def input_validation_string(min_len):
+
+    try:
+        min_len = int(min_len)
+        if min_len < 0:
+            return print('O parâmetro de tamanho mínimo da string deve ser um inteiro positivo ou igual a zero!')
+
+    except:
+        return print('O parâmetro de tamanho mínimo da string deve ser um inteiro!')
+
+
+    value = input()
+
+    while len(value) < min_len:
+        print(f'Valor inválido, número de caracteres mínimo deve ser {min_len}! Digite o valor corretamente.')
+        value = input()
+
+    return value
+
+def input_validation_int():
+
+    value = input()
+    while not isinstance(value, int):
+
+        try:
+            value = int(value)
+            assert value >= 0
+
+        except:
+            print('O valor deve ser um inteiro positivo ou igual a zero!')
+            print('Digite o valor novamente:')
+            value = input()
+
+    return value
+
+
 def fetch_players():
 
     lista_jogadores = []
 
     try:
         with open(PLAYERS_ARCHIVE, "r", encoding="utf-8") as f:
-            dados_do_arquivo = json.load(f) 
+            dados_do_arquivo = json.load(f)
             
             for dados in dados_do_arquivo:
                 jogador = Player(dados["id"], dados["name"])
-                
 
                 jogador.victories = dados["victories"]
                 jogador.defeats = dados["defeats"]
                 jogador.draws = dados["draws"]
                 jogador.score = dados["score"]
+
+                for h in dados["history"]:
+                    history = History(h["match_id"], h["oponent_id"], h["oponent_name"], h["result"])
+                    jogador.history.append(history)
                 
                 lista_jogadores.append(jogador)
                 
@@ -64,6 +103,165 @@ def fetch_matchs():
         
     return matchs_list
 
+def register_player(name):
+
+    new_players = []
+    jogadores_registrados = fetch_players()
+
+    if len(jogadores_registrados) > 0:
+
+        for jogador in jogadores_registrados:
+
+            if jogador.name == name:
+                name = input('Nome já existe, digite um nome novamente!')
+
+        id_max = 0
+
+        for jogador in jogadores_registrados:
+
+            if jogador.id > id_max:
+                id_max = jogador.id
+
+        jogador = Player(id_max + 1, name)
+
+        new_players = [jogador.for_dict() for jogador in jogadores_registrados]
+        new_players.append(jogador.for_dict())
+
+    else:
+        jogador = Player(1, name)
+        new_players = [jogador.for_dict()]
+
+    try:
+        with open(PLAYERS_ARCHIVE, "w") as f:
+            json.dump(new_players, f, indent=4)
+        
+        print('\nJogador cadastrado com sucesso!')
+
+    except:
+        return print('Erro ao cadastrar jogador!')
+    
+
+def register_match(player1_id, player2_id):
+
+    new_matchs = []
+    new_players = []
+    winner_id = 0
+    result_player_1 = ''
+    result_player_2 = ''
+    players = fetch_players()
+    register_matchs = fetch_matchs()
+
+    player1 = False
+    player2 = False
+
+    for player in players:
+
+        if player.id == int(player1_id):
+            player1 = player
+        
+        if player.id == int(player2_id):
+            player2 = player
+
+    if player1 != False and player2 != False:
+
+        draw = input('Houve empate? (s/n)')
+
+        if draw.lower() == 's':
+            draw = True
+            player1.draws += 1
+            player1.score += 1
+            player2.draws += 1
+            player2.score += 1
+            result_player_1 = 'Empatou'
+            result_player_2 = 'Empatou'
+
+        else:
+            draw = False
+            print(f'Jogador 1: {player1.name} | Jogador 2: {player2.name}')
+            winner = input('Qual jogador que venceu? (1/2)')
+
+            if winner == '1':
+                winner_id = player1_id
+                player1.victories += 1
+                player1.score += 3
+                player2.defeats += 1
+                player2.score -= 1 if player2.score > 0 else 0
+                result_player_1 = 'Ganhou'
+                result_player_2 = 'Perdeu'
+
+            else:
+                winner_id = player2_id                           
+                player2.victories += 1
+                player2.score += 3
+                player1.defeats += 1
+                player1.score -= 1 if player1.score > 0 else 0 
+                result_player_1 = 'Perdeu'
+                result_player_2 = 'Ganhou'
+
+        
+        id_max = 0
+        if len(register_matchs) > 0:
+            for match in register_matchs:
+                if match.id > id_max:
+                    id_max = match.id
+
+        new_match = Match(id_max + 1, player1_id, player2_id, draw, winner_id)
+        new_matchs = [match.for_dict() for match in register_matchs]
+        new_matchs.append(new_match.for_dict())
+
+        history_player1 = History(new_match.id, player2_id, player2.name, result_player_1)
+        history_player2 = History(new_match.id, player1_id, player1.name, result_player_2)
+
+        player1.history.append(history_player1)
+        player2.history.append(history_player2)
+
+        for player in players:
+
+            if player.id != int(player1_id) and player.id != int(player2_id):
+                new_players.append(player.for_dict())
+        
+        new_players.append(player1.for_dict())
+        new_players.append(player2.for_dict())
+
+        with open(MATCHS_ARCHIVE, "w") as f:
+            json.dump(new_matchs, f, indent=4)
+
+        with open(PLAYERS_ARCHIVE, "w") as f:
+            json.dump(new_players, f, indent=4)
+    else:
+        print('Ids especificados não existem!')
+
+def get_player(id):
+
+    players = fetch_players()
+    exists_player = False
+    for player in players:
+        if player.id == int(id):
+
+            exists_player = True
+            print(f'Id: {player.id}')
+            print(f'Nome: {player.name}')
+            print(f'Vitórias: {player.victories}')
+            print(f'Derrotas: {player.defeats}')
+            print(f'Empates: {player.draws}')
+            print(f'Pontuação: {player.score}')
+
+            print('Histórico de partidas disputadas:')
+
+            if len(player.history) > 0:
+
+                print("\n" + "=" * 32)
+                print(f"{'Adversário':<20} {'Resultado':<10}")
+                print("=" * 32)
+
+                for h in player.history:
+                    print(f"{h.oponent_name:<20} {h.result:<10}")
+                print("=" * 32)
+            else:
+                print("Sem partidas disputadas por esse jogador!")
+
+    if not exists_player:
+        print('O jogador especificado não existe!')
 
 def ranking_players(players):
 
@@ -76,9 +274,9 @@ def ranking_players(players):
                 min = j
             elif players[j].score == players[min].score and players[j].victories > players[min].victories:
                 min = j
-            elif players[j].victories == players[min].victories and players[j].draws > players[min].draws:
+            elif players[j].score == players[min].score and players[j].victories == players[min].victories and players[j].draws > players[min].draws:
                 min = j
-            elif players[j].draws == players[min].draws and players[j].defeats < players[min].defeats:
+            elif players[j].score == players[min].score and players[j].victories == players[min].victories and players[j].draws == players[min].draws and players[j].defeats < players[min].defeats:
                 min = j
 
         players[i], players[min] = players[min], players[i]
@@ -94,9 +292,24 @@ class Player:
         self.defeats = 0
         self.draws = 0
         self.score = 0
+        self.history = []
     
     def for_dict(self):
-        return self.__dict__
+
+        history_dict = []
+        for h in self.history:
+            history_dict.append(h.for_dict())
+
+        return {
+
+            "id": self.id,
+            "name": self.name,
+            "victories": self.victories,
+            "defeats": self.defeats,
+            "draws": self.draws,
+            "score": self.score,
+            "history": history_dict
+        }
 
 class Match:
 
@@ -106,6 +319,17 @@ class Match:
         self.player2_id = player2_id
         self.draw = draw
         self.winner_id = winner_id
+    
+    def for_dict(self):
+        return self.__dict__
+
+class History:
+
+    def __init__(self, match_id, oponent_id, oponent_name, result):
+        self.match_id = match_id
+        self.oponent_id = oponent_id
+        self.oponent_name = oponent_name
+        self.result = result
     
     def for_dict(self):
         return self.__dict__
@@ -128,154 +352,47 @@ while system_on:
     match option:
 
         case "1":
-            new_players = []
-            jogadores_registrados = fetch_players()
-            name = input("Digite o nome do jogador: ")
 
-            if len(jogadores_registrados) > 0:
-
-                for jogador in jogadores_registrados:
-
-                    if jogador.name == name:
-                        name = input('Nome já existe, digite um nome novamente!')
-
-                id_max = 0
-
-                for jogador in jogadores_registrados:
-
-                    if jogador.id > id_max:
-                        id_max = jogador.id
-
-                jogador = Player(id_max + 1, name)
-
-                new_players = [jogador.for_dict() for jogador in jogadores_registrados]
-                new_players.append(jogador.for_dict())
-
-            else:
-                jogador = Player(1, name)
-                new_players = [jogador.for_dict()]
-
-            with open(PLAYERS_ARCHIVE, "w") as f:
-                json.dump(new_players, f, indent=4)
+            print("Preciso de um nome com no mínimo 3 caracteres para registrar.")
+            print("Digite o nome do jogador:")
+            name = input_validation_string(3)
+            register_player(name)
 
         case "2":
 
-            new_matchs = []
-            new_players = []
-            players = fetch_players()
-            winner_id = 0
-            register_matchs = fetch_matchs()
-
-            player1_id = input('Digite o id do primeiro jogador: ')
-            player2_id = input('Digite o id do segundo jogador: ')
-
-            player1 = False
-            player2 = False
-
-            for player in players:
-
-                if player.id == int(player1_id):
-                    player1 = player
-                
-                if player.id == int(player2_id):
-                    player2 = player
-
-            if player1 != False and player2 != False:
-
-                draw = input('Houve empate? (s/n)')
-
-                if draw.lower() == 's':
-                    draw = True
-                    player1.draws += 1
-                    player1.score += 1
-                    player2.draws += 1
-                    player2.score += 1
-
-                else:
-                    draw = False
-                    print(f'Jogador 1: {player1_id} | Jogador 2: {player2_id}')
-                    winner = input('Qual jogador que venceu? (1/2)')
-
-                    if winner == '1':
-                        winner_id = player1_id
-                        player1.victories += 1
-                        player1.score += 3
-                        player2.defeats += 1
-                        player2.score -= 1 if player2.score > 0 else 0
-
-                    else:
-                        winner_id = player2_id                           
-                        player2.victories += 1
-                        player2.score += 3
-                        player1.defeats += 1
-                        player1.score -= 1 if player1.score > 0 else 0 
-
-                
-                id_max = 0
-                if len(register_matchs) > 0:
-                    for match in register_matchs:
-                        if match.id > id_max:
-                            id_max = match.id
-
-                new_match = Match(id_max + 1, player1_id, player2_id, draw, winner_id)
-                new_matchs = [match.for_dict() for match in register_matchs]
-                new_matchs.append(new_match.for_dict())
-
-                for player in players:
-
-                    if player.id != int(player1_id) and player.id != int(player2_id):
-                        new_players.append(player.for_dict())
-                
-                new_players.append(player1.for_dict())
-                new_players.append(player2.for_dict())
-
-                with open(MATCHS_ARCHIVE, "w") as f:
-                    json.dump(new_matchs, f, indent=4)
-
-                with open(PLAYERS_ARCHIVE, "w") as f:
-                    json.dump(new_players, f, indent=4)
-            else:
-                print('Ids especificados não existem!')
-
+            print("Preciso do id de dois jogadores para registrar a partida.")
+            print('Digite o id do primeiro jogador: ')
+            player1_id = input_validation_int()
+            print('Digite o id do segundo jogador: ')
+            player2_id = input_validation_int()
+            register_match(player1_id, player2_id)
 
         case "3":
 
-            players = fetch_players()
-            id = input('Digite o id do jogador que deseja consultar: ') # Validar inteiros
-
-            exists_player = False
-            for player in players:
-                if player.id == int(id):
-                    exists_player = True
-                    print(f'Id: {player.id}')
-                    print(f'Nome: {player.name}')
-                    print(f'Vitórias: {player.victories}')
-                    print(f'Derrotas: {player.defeats}')
-                    print(f'Empates: {player.draws}')
-                    print(f'Pontuação: {player.score}')
-
-            if not exists_player:
-                print('O jogador especificado não existe!')
+            print('Digite o id do jogador que deseja consultar:')
+            id = input_validation_int()
+            get_player(id)
             
         case "4":
+            
             players = fetch_players()
-            ranking = ranking_players(players)
-
+            ranking_players(players)
+            
             print('RANKING DE JOGADORES')
-
             print("\n" + "=" * 80)
-            print(f"{'Jogador':<20} {'Pontos':<10} {'Vitórias':<10} {'Empates':<10} {'Derrotas':<10}")
+            print(f"{'Posição':<10} {'Jogador':<20} {'Pontos':<10} {'Vitórias':<10} {'Empates':<10} {'Derrotas':<10}")
             print("=" * 80)
-            for player in players:
+            for i in range(len(players)):
 
                 print(
-                    f"{player.name:<20} "
-                    f"{player.score:<10} "
-                    f"{player.victories:<10} "
-                    f"{player.draws:<10} "
-                    f"{player.defeats:<10}"
+                    f"{i + 1:<10} "
+                    f"{players[i].name:<20} "
+                    f"{players[i].score:<10} "
+                    f"{players[i].victories:<10} "
+                    f"{players[i].draws:<10} "
+                    f"{players[i].defeats:<10}"
                 )
-            print("\n" + "=" * 80)
+            print("=" * 80)
 
         case "5":
             print("Imprimir Relatório")
